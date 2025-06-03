616f77a2a4b87979384e10c3472221f0669cc888
= <=>
2025-06-03 15:39:17 +0100

    Added documentation and refactored code

--- app/users/authid.py
+++ app/users/authid.py
@@ -1,12 +1,24 @@
 from app.database.db_connect import connect
 
 def authenticate(email:str):
+    """
+    A function to authenticate users, by returning the user ID
+    by querying the database with the provided email.
+
+    Args:
+        email (str): The user's email
+
+    Returns:
+        int: The user ID (uID)
+    """
 
     if not email:
         print("ERROR: Email not supplied")
+        return None
         
     connection = connect()
     cursor = connection.cursor()
+
     query = f"""
         SELECT u.uID, u.email 
         FROM Users u 
@@ -14,11 +26,11 @@ def authenticate(email:str):
         GROUP BY u.uID
     """
     cursor.execute(query)
+
     user_id:int = int(cursor.fetchone()[0])
+
     if (user_id):
-        print(f"Email: {email} -> ID: {user_id}")
         return user_id
     else:
-        print("ERROR: Email does not match any ID records")
-
-authenticate("test@test.com")
+        print("ERROR: Email does not match uID records in database")
+        return None
