0453d3efdd6baef9678275566387100a8ab92846
= <=>
2025-06-03 12:22:40 +0100

    Testing db connection on server

--- server/database/connect.py
+++ server/database/connect.py
@@ -1,8 +1,27 @@
 from flask import Blueprint
+import mysql.connector
+import os
+from dotenv import load_dotenv
 
+def connect_to_db():
+    load_dotenv()
+    cnx = mysql.connector.connect (
+        user=os.getenv('DB_USER'),
+        password= os.getenv('DB_PASS'),
+        host=os.getenv('HOST'),
+        database= os.getenv('DATABASE')
+    )
 
-db_connect = Blueprint('connect', __name__)
+    if (cnx):
+        print("Connection Successful")
+    else:
+        print("Connection failed")
+    cnx.close()
 
-@db_connect.route('/connect', methods=('GET', 'POST'))
-def connect():
-    return "TESTING..."
+connect_to_db()
+
+# db_connect = Blueprint('connect', __name__)
+
+# @db_connect.route('/connect', methods=('GET', 'POST'))
+# def connect():
+#     return "TESTING..."
