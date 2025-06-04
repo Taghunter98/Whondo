0873462ded3d8b17efd91ea1345cd2ce66792941
= <=>
2025-06-04 08:22:04 +0100

    Added integer type checking

--- app/tests/test_authId.py
+++ app/tests/test_authId.py
@@ -6,7 +6,11 @@ from app.users.authid import authenticate
 class TestAuthID(unittest.TestCase):
 
     @unittest.skipIf(os.getenv("CI"), "Skipping test in CI pipeline")
-    def testAuthRed(self):
+    def testIncorrectAuth(self):
+        """
+        Test method tests if authenticate returns the correct None value
+        for an email that is not in the database.
+        """
 
         user:str = "josh@test.com"
         result = authenticate(user)
@@ -14,9 +18,14 @@ class TestAuthID(unittest.TestCase):
         self.assertIsNone(result, "ERROR: Function is not handling error correctly")
     
     @unittest.skipIf(os.getenv("CI"), "Skipping test in CI pipeline")
-    def testAuthGreen(self):
+    def testCorrectAuth(self):
+        """
+        Test method tests if authenticate returns a valid result that is not None
+        and is an integer.
+        """
 
         user:str = "test@test.com"
         result = authenticate(user)
         
-        self.assertNotEqual(result, -1, "ERROR: Function is not returning valid uID")
+        self.assertNotEqual(result, None, "ERROR: Function is not returning valid uID")
+        self.assertIs(type(result), "<class 'int'>")
