7e0d22c9135af8f9c3253f6cbd464cbfea0acb06
= <=>
2025-06-04 14:41:38 +0100

    Testing API

--- app/users/login.py
+++ app/users/login.py
@@ -8,24 +8,26 @@ login_bp = Blueprint("login_bp", __name__)
 
 @login_bp.route('/login/auth', methods = ['POST'])
 def login():
+    data         = request.get_json()
+    email:str    = data.get['email']
+    password:str = data.get['password']
 
-    email:str    = request.args['email']
-    password:str = request.args['password']
-
-    if not email or password:
+    if not email or  not password:
         return jsonify({
             "error": "ERROR: User email or password not provided"
-        })
+        }), 400
     
     user_id:int = authenticate(email)
 
     if (user_id is None):
         return jsonify({
             "error": "ERROR: User email does not match database records"
-        })
+        }), 401
     
-    connecttion = connect()
-    cursor      = connecttion.cursor()
+    connection = connect()
+    cursor     = connection.cursor()
+    cursor.close()
+    connection.close()
 
     query:str = f"""
         SELECT u.uID, u.password
@@ -37,10 +39,18 @@ def login():
     result = cursor.fetchone()
 
     if (result is not None):
-        hash_string:str = result[0]
+        hash_string:str = result[1]
         valid:bool = check_password(password, hash_string)
         if (valid is True):
             return jsonify({
                 "message": f"SUCCESS: User {email} logged in successfully",
-                "status": valid
-            })
+                "status": True
+            }), 200
+        else:
+            return jsonify({
+                "error":"ERROR: Incorrect Password"
+            }), 401
+    else:
+        return jsonify({
+                "error":"ERROR: User not found"
+            }), 404
