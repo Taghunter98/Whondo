c475fb6e7626d661f7a22b4764e97df8d07ac930
= <=>
2025-06-05 17:03:47 +0100

    Testing api

--- app/tests/test_login_api.py
+++ app/tests/test_login_api.py
@@ -21,7 +21,7 @@ class TestLoginAPI(unittest.TestCase):
 
         return requests.post(API_URL, json = API_DATA,)
     
-    def testValidRequest(self):
+    def testResponse(self):
         """
         Test method tests if request is a valid object and code 200 OK.
         """
@@ -31,7 +31,7 @@ class TestLoginAPI(unittest.TestCase):
         self.assertIsNotNone(data, "Request is not returning valid object")
         self.assertEqual(data.status_code, 200, f"Request is returing code: {data.status_code}")
 
-    def testRequestContent(self):
+    def testStatus(self):
         """
         Test method tests if request returns a valid status of true.
         """
@@ -39,6 +39,8 @@ class TestLoginAPI(unittest.TestCase):
         data = self.setUpClass()
 
         self.assertEqual(data.json()['status'], True)
+
+    
 
 class TestLoginAPIFail(unittest.TestCase):
 
@@ -59,7 +61,7 @@ class TestLoginAPIFail(unittest.TestCase):
 
         return requests.post(API_URL, json = API_DATA,)
     
-    def testInvalidResponse(self):
+    def testResponse(self):
 
         data = self.setUpClass()
 
@@ -71,3 +73,34 @@ class TestLoginAPIFail(unittest.TestCase):
         data = self.setUpClass()
 
         self.assertEqual(data.json()["error"], "User email does not match database records", f"Request is returing: {data.json()['error']}")
+
+class TestInvalidData(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Test method sets up test by calling the API.
+
+        Returns:
+            object: Request object
+        """
+        
+        API_URL:str = "https://whondo.com/login/auth"
+        API_DATA = {
+            "email": "",
+            "password": ""
+        }
+
+        return requests.post(API_URL, json = API_DATA,)
+    
+    def testResponse(self):
+
+        data = self.setUpClass()
+
+        self.assertEqual(data.status_code, 400, f"Request is returing code: {data.status_code}")
+
+    def testErrorMessage(self):
+
+        data = self.setUpClass()
+
+        self.assertEqual(data.json()["error"], "User email or password not provided", f"Request is returing: {data.json()['error']}")
