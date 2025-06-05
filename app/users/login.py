b6cf4833fcc9f1dd61117482a96444765ab32bea
= <=>
2025-06-05 08:21:41 +0100

    saving changes

--- app/users/login.py
+++ app/users/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, session, jsonify
 
 from .authid import authenticate
 from app.database.db_connect import connect
@@ -8,21 +8,29 @@ login_bp = Blueprint("login_bp", __name__)
 
 @login_bp.route('/login/auth', methods = ['POST'])
 def login():
+    """
+    The REST API is responsibe for logging in the user from an external
+    POST request with the user's email and plaintext password.
+
+    The data is
+
+    Returns:
+        json: json response of successs or appropriate error message
+    """    
+
     data         = request.get_json()
     email:str    = data.get('email')
     password:str = data.get('password')
 
     if not email or  not password:
-        return jsonify({
-            "error": "ERROR: User email or password not provided"
-        }), 400
+        return jsonify({"error" : "ERROR: User email or password not provided"}), 400
     
     user_id:int = authenticate(email)
 
     if (user_id is None):
-        return jsonify({
-            "error": "ERROR: User email does not match database records"
-        }), 401
+        return jsonify({"error" : "ERROR: User email does not match database records"}), 401
+    
+    session['uID'] = user_id
     
     connection = connect()
     cursor     = connection.cursor()
@@ -44,14 +52,13 @@ def login():
         valid:bool = check_password(password, hash_string)
         if (valid is True):
             return jsonify({
-                "message": f"SUCCESS: User {email} logged in successfully",
-                "status": True
+                "message" : f"SUCCESS: User {email} logged in successfully",
+                "status"  : True
             }), 200
         else:
             return jsonify({
-                "error":"ERROR: Incorrect Password"
+                "error"  : "ERROR: Incorrect Password",
+                "status" : False
             }), 401
     else:
-        return jsonify({
-                "error":"ERROR: User not found"
-            }), 404
+        return jsonify({"error":"ERROR: User not found"}), 404
