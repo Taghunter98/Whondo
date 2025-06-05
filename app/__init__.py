3d0c0b133345c5a977c2e7a9b3a89b035ecf485d
= <=>
2025-06-05 11:04:35 +0100

    Fixing bug with POST request

--- app/__init__.py
+++ app/__init__.py
@@ -3,34 +3,19 @@ from dotenv import load_dotenv
 from flask import Flask, session, redirect
 from flask_session import Session
 
-from logging.config import dictConfig
-
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
-
+from .logging_setup import setup_logging
+import logging
 
 def create_app():
 
+    setup_logging()
     
     app = Flask(__name__, instance_relative_config=True)
 
     load_dotenv()
     SECRET_KEY = os.getenv('SECRET_KEY')
     if (not SECRET_KEY):
-        app.logger.error("No SECRET_KEY variable found")
+        logging.error("No SECRET_KEY variable found")
         return None
     
     app.config.from_mapping(
@@ -38,9 +23,9 @@ def create_app():
     )
     app.config["SESSION_PERMANENT"]       = False 
     app.config["SESSION_TYPE"]            = "filesystem"
-    app.config["SESSION_COOKIE_SECURE"]   = True
-    app.config["SESSION_COOKIE_HTTPONLY"] = True
-    app.config["SESSION_COOKIE_SAMESITE"] = True
+    # app.config["SESSION_COOKIE_SECURE"]   = True
+    # app.config["SESSION_COOKIE_HTTPONLY"] = True
+    # app.config["SESSION_COOKIE_SAMESITE"] = True
     
     Session(app)
     
@@ -63,5 +48,5 @@ def create_app():
         else:
             return "Please log in"
         
-    app.logger.info("Flask app created successfully")
+    logging.info("Flask app created successfully")
     return app
