a9fa6b458fc269eba412fb0052f0a07352c5b804
= <=>
2025-06-05 16:45:07 +0100

    Testing api

--- app/tests/test_login_api.py
+++ app/tests/test_login_api.py
@@ -6,16 +6,18 @@ class TestLoginAPI(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        
+        URL:str = "https://whondo.com/login/auth"
+        DATA = {
+            "email": "test@test.com",
+            "password": "testing123"
+        }
 
-        return requests.post(
-            "https://whondo.com/login/auth",
-            data = {"email":"test@test.com", "password": "password123"},
-            headers = {"Content-Type":"application/json"}
-        )
+        return requests.post(URL, json = DATA,)
     
     def testValidRequest(self):
 
         data = self.setUpClass()
-        
+        print(data.error)
         self.assertIsNotNone(data, "Request is not returning valid object")
-        self.assertEqual(data.status_code, 200, f"Request is returing code: {data.status_code} data: {data.error}")
+        self.assertEqual(data.status_code, 200, f"Request is returing code: {data.status_code}")
