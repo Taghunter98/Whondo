9f3bd37af640c6cdfc5e8de6eaa0a9892f9deb98
= <=>
2025-06-05 14:18:30 +0100

    Added documenation for create_app

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,38 @@ def create_app():
     App is created with instance relative config set to True, tells the app
     that the cofig files are relative to the instance folder.
 
-    Log file path is located and then converted into an absolute path (e.g.
-    /home/ec2-user/Whondo/app.log). Directory name containing file is
-    fetched and .. moves up one directory to Whondo/ the root directory.
+    Flask app is configured with NGINX reverse proxy, so ProxyFix is used to
+    provide accurate ip addresses for logging purposes.
 
-    Log file path is 
+    Log file path is defined and 'app.log' is created if not already present.
 
+    Log file config is setup, default formatter provides statndard logging
+    output, special request logging format is defined in the RequestFormatter
+    class which is taken from Flask's documentation. The handlers are setup to
+    define where the logs are written, wsgi sends logs to Flask/Werkzeug output
+    the INFO logs are output to 'app.log' in the root directory. Root logger
+    sets up global logging for the Flask app, all log messages are sent to 
+
+    Log file formatter to handle HTTP requests is setup with RequestFormatter class.
+
+    Default handler is set to the new HTTP request handler style.
+
+    SECRET_KEY is found and verified in the environment variable.
+
+    Flask app config is setup to handle sessions, the config handles:
+        - Sessions are active whilst the browser is open
+        - Cookie data is stored on the server
+        - Sessions are encrypted
+        - HTTPS only for cookies
+
+    Session is started with the Flask app.
+
+    Bluprints are imported and registered.
+
+    Logging before and after logs the incomming request with correct ip addresses,
+    request method and response.
+
+    The root directory '/' is served .
     """
 
     app = Flask(__name__, instance_relative_config=True)
@@ -59,12 +85,14 @@ def create_app():
     })
 
     formatter = RequestFormatter(
-    '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
-    '%(levelname)s in %(module)s: %(message)s'
-)
+        '[%(asctime)s] %(remote_addr)s requested %(url)s\n'
+        '%(levelname)s in %(module)s: %(message)s'
+    )
+
     default_handler.setFormatter(formatter)
 
     load_dotenv()
+
     SECRET_KEY = os.getenv('SECRET_KEY')
     if (not SECRET_KEY):
         logging.error("No SECRET_KEY variable found")
@@ -82,6 +110,7 @@ def create_app():
     Session(app)
     
     from .users.login import login_bp
+
     app.register_blueprint(login_bp)
 
     @app.before_request
@@ -93,7 +122,6 @@ def create_app():
         app.logger.info(f"Response: {response.status} for {request.method} {request.path}")
         return response
 
-    # Base directory for Whondo - currently just says hello world :)
     @app.route('/')
     def hello():
         if (session.get("uID")):
@@ -101,18 +129,31 @@ def create_app():
             return f"Hello, World!\nUser logged in id: {session_id}"
         else:
             return "Hello World!"
-    
-    @app.route('/login')
-    def check_session():
-        if (session.get("uID")):
-            return redirect("/")
-        else:
-            return "Please log in"
         
     return app
 
 class RequestFormatter(logging.Formatter):
+    """
+    A class that creates a formatter for HTTP requests.
+
+    Args:
+        logging (object): Formatter object for logs
+    """
+
     def format(self, record):
+        """
+        A function that configures a formatter for HTTP requests.
+
+        If request has context, then the url and address are set.
+        Otherwise they are set to None.
+
+        Args:
+            record (object): Request object containing data
+
+        Returns:
+            formatter object: Logger entry   
+        """
+
         if has_request_context():
             record.url = request.url
             record.remote_addr = request.remote_addr
