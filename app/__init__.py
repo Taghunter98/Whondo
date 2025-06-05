443844bb570a2c6d17be9e9903da88f7a2378fb2
= <=>
2025-06-05 09:04:23 +0100

    Testing session functionality

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,28 @@
 import os
-from flask import Flask, session
+from flask import Flask, session, redirect
 
 def create_app():
+    
     app = Flask(__name__, instance_relative_config=True)
 
+    app.config["SESSION_PERMANENT"] = False 
+    app.config["SESSION_TYPE"] = "filesystem"
+    
     from .users.login import login_bp
     app.register_blueprint(login_bp)
 
     # Base directory for Whondo - currently just says hello world :)
     @app.route('/')
     def hello():
-        return 'Hello, World!'
+        if (session.get("uID")):
+            return f"Hello, World!\nUser logged in id: {session.get("uID")}"
+        else:
+            return "Hello World!"
     
     @app.route('/login')
     def check_session():
-        if (session['uID']):
-            return f"User {session['uID']} is logged in"
+        if (session.get("uID")):
+            return redirect("/")
         else:
             return "Please log in"
 
