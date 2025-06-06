b9719ae6cf53e9d8d29c6ce253cb752f73d07c2d
= <=>
2025-06-06 18:14:46 +0100

    Testing image storage

--- app/users/login.py
+++ app/users/login.py
@@ -9,7 +9,7 @@ Version:     1.0
 Description: Serves a Blueprint API for logging in and verifying users. 
 """
 
-from flask import Blueprint, request, session, jsonify, current_app
+from flask import Blueprint, request, session, jsonify, current_app, render_template
 
 from .authid                 import authenticate
 from app.database.db_connect import connect
@@ -17,7 +17,7 @@ from app.security.hashing    import check_password
 
 login_bp = Blueprint("login_bp", __name__)
 
-@login_bp.route('/login/auth', methods = ['POST'])
+@login_bp.route('/login/auth', methods = ['POST', 'GET'])
 def login():
     """
     The REST API is responsibe for logging in the user from an external POST
@@ -36,51 +36,54 @@ def login():
         json: json response of successs or appropriate error message
     """    
 
-    data         = request.get_json()
-    email:str    = data.get('email')
-    password:str = data.get('password')
+    if request.method == 'POST':
+        data         = request.get_json()
+        email:str    = data.get('email')
+        password:str = data.get('password')
 
-    if (not email or not password):
-        return jsonify({"error" : "User email or password not provided"}), 400
-    
-    user_id:int = authenticate(email)
+        if (not email or not password):
+            return jsonify({"error" : "User email or password not provided"}), 400
+        
+        user_id:int = authenticate(email)
 
-    if (user_id is None):
-        return jsonify({"error" : "User email does not match database records"}), 401
-    
-    connection = connect()
-    cursor     = connection.cursor()
+        if (user_id is None):
+            return jsonify({"error" : "User email does not match database records"}), 401
+        
+        connection = connect()
+        cursor     = connection.cursor()
 
-    query:str = """
-        SELECT u.uID, u.password
-        FROM Users u
-        WHERE u.uID = %d;
-    """
+        query:str = """
+            SELECT u.uID, u.password
+            FROM Users u
+            WHERE u.uID = %d;
+        """
 
-    cursor.execute(query, (user_id))
-    result = cursor.fetchone()
+        cursor.execute(query, (user_id))
+        result = cursor.fetchone()
 
-    cursor.close()
-    connection.close()
+        cursor.close()
+        connection.close()
 
-    if (result is not None):
-        hash_string:str = result[1]
-        valid:bool = check_password(password, hash_string)
+        if (result is not None):
+            hash_string:str = result[1]
+            valid:bool = check_password(password, hash_string)
 
-        if (valid is True):
-            session['uID'] = user_id
-            current_app.logger.info(f"User authenticated, starting new Session")
-            
-            return jsonify({
-                "message" : f"User: {email} logged in successfully",
-                "status"  : True
-            }), 200
+            if (valid is True):
+                session['uID'] = user_id
+                current_app.logger.info(f"User authenticated, starting new Session")
+                
+                return jsonify({
+                    "message" : f"User: {email} logged in successfully",
+                    "status"  : True
+                }), 200
+            else:
+                current_app.logger.error(f"User: {email} access denied, incorrect password")
+                return jsonify({
+                    "error"  : "Incorrect password",
+                    "status" : False
+                }), 401
         else:
-            current_app.logger.error(f"User: {email} access denied, incorrect password")
-            return jsonify({
-                "error"  : "Incorrect password",
-                "status" : False
-            }), 401
+            current_app.logger.error(f"User: {email} not found")
+            return jsonify({"error":"User not found"}), 404
     else:
-        current_app.logger.error(f"User: {email} not found")
-        return jsonify({"error":"User not found"}), 404
+        return render_template("login.html")
