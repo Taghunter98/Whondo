dc52b0b8e7a0374d8dfed444bec9693bc4e33ce2
= <=>
2025-06-08 09:58:34 +0100

    Testing mimetypes

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,6 @@ Version:     1.0
 Description: Creates a Flask app instance and sets up logging.
 """
 
-import mimetypes
-mimetypes.add_type('application/javascript', '.js')
-mimetypes.add_type('text/css', '.css')
-
 from flask                         import Flask, session, redirect, has_request_context, request
 from flask_session                 import Session
 from flask.logging                 import default_handler
