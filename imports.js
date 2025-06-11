4e4428b41ba0da6c6833642c530e7f2d24950070
= <=>
2025-06-11 13:49:01 +0100

    Fixed imports

--- imports.js
+++
@@ -1,4 +0,0 @@
-import './app/static/jay-comps/comp-src/comp.js';
-import './app/static/jay-comps/comps/button.js';
-import './app/static/jay-comps/comps/card.js';
-import './app/static/jay-comps/comp-pages/login.js';
