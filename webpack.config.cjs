78893c79ef5f334eb4b410f2067401b8101f13c3
= <=>
2025-06-17 19:20:21 +0100

    webpack builds to static for flask to serve

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,9 +1,10 @@
 const path = require('path');
+
 module.exports = {
-    entry: "./app/static/jay-comps/imports.js",
-    mode: 'development',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  entry: "./app/static/jay-comps/imports.js",
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'app', 'static')
+  }
 };
