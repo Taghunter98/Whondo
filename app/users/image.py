b82a242226a0cabda50ac0bcfa2a1b608a90a2de
= <=>
2025-06-18 13:42:26 +0100

    Testing image

--- app/users/image.py
+++ app/users/image.py
@@ -1,8 +1,7 @@
-from flask import Blueprint, send_from_directory, current_app
+from flask import Blueprint, send_from_directory, current_app, request
 
 image_bp = Blueprint("image_bp", __name__)
 
 @image_bp.route("/uploads/<path>")
 def download_file():
-    path = "/home/ec2-user/Uploads/Profile/bassettjosh397@gmail.com/2025-06-07_bassettjosh397@gmail.com_wilson.jpg"
-    return send_from_directory(current_app.config["UPLOAD_FOLDER"], path=path, as_attachment=True)
+    return send_from_directory(current_app.config["UPLOAD_FOLDER"], path=request.args.get['path'], as_attachment=True)
