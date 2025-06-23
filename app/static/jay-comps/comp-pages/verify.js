1d53f921c0aa4af0cf6828f043fedb331f85b4d4
Sarit Samkumpim <saritsamkumpim@gmail.com>
2025-06-23 12:47:41 +0100

    update

---
+++ app/static/jay-comps/comp-pages/verify.js
@@ -0,0 +1,15 @@
+import { Comp } from 'jay-comp';
+
+class VerifyPage extends Comp {
+    
+    constructor() {
+
+        super();
+
+        this.name_ = "Verify Page";
+        this.html  = this.createHTML();
+        this.css_  = this.createCSS();
+    
+    }
+
+}
