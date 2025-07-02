5178ebb00d93ac8f6e8b88224bdc2c0fcaeef9ef
Sarit Samkumpim <saritsamkumpim@gmail.com>
2025-07-02 02:09:12 +0100

    update verified page

--- app/static/jay-comps/comp-pages/verified.js
+++ app/static/jay-comps/comp-pages/verified.js
@@ -20,10 +20,11 @@ class Verified extends Comp {
         <div class="background">
             
             <div class="container">
-                <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="64px" viewBox="0 -960 960 960" width="64px" fill="currentColor"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                <!-- <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="64px" viewBox="0 -960 960 960" width="64px" fill="currentColor"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
                 <h3 class="head">Verified!</h3>
-                <p class="describe">Welcome {{ email }}!<br>Your account has now been verified.</p>
-                <comp-button></comp-button>
+                <p class="describe">Welcome {{ email }}!<br>Your account has now been verified.</p> -->
+                <dialog-box id="describe"></dialog-box>
+
             </div>
         </div>
         `;
@@ -42,10 +43,9 @@ class Verified extends Comp {
             flexDirection: "column",
             width: "100%",
             alignItems: "centre",
-            border: false,
-            gap: 0,
             background: "black10",
             justifyContent: "centre",
+            overflow: "hidden",
         });
 
         const backgroundMobile = this.design.create({
@@ -57,30 +57,9 @@ class Verified extends Comp {
         const container = this.design.create({
             class: "container",
             display: "flex",
-            flexDirection: "column",
-            width: "auto",
-            maxWidth: 500,
-            padding: 20,
-            alignItems: "centre",
-            border: "border",
-            borderRadius: 16,
-            gap: 10,
             background: "white",
-            textAlign: "start",
             animation: prop,
-            marginTop: 0,
-        });
-
-        const head = this.design.create({
-            class: "head",
-            fontSize: 24,
-            alignSelf: "centre",
-            lineHeight: 30,
-        });
-
-        const icon = this.design.create({
-            class: "icon",
-            colour: "green60"
+            width: "auto",
         });
 
         
@@ -88,8 +67,6 @@ class Verified extends Comp {
         ${effect}
         ${background}
         ${container}
-        ${icon}
-        ${head}
         
 
         @media (max-width: 600px) {
@@ -102,9 +79,16 @@ class Verified extends Comp {
     }
 
     hook() {
+        
+        const describe = this.shadowRoot.getElementById("describe");
+        const btn      = this.shadowRoot.getElementById('btn');
 
-        this.shadowRoot.querySelector('comp-button').buttonText    = "Continue";
-        this.shadowRoot.querySelector('comp-button').buttonVarient = 4;
+        describe.svgIcon   = `xmlns="http://www.w3.org/2000/svg" height="64px" viewBox="0 -960 960 960" width="64px" fill="currentColor"><path d="m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/`;
+        describe.title     = "Verified!";
+        describe.paragraph = `Welcome {{ email }}!<br>Your account has now been verified.`;
+    
+        btn.buttonText_   = "Continue";
+        btn.buttonVarient = 4;
     
     }
 
