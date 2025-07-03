331ab1e4cfd1f616153e7b32bb68c809ca22bd61
= <=>
2025-07-03 14:43:03 +0100

    Fixed sql query

--- app/property/advert.py
+++ app/property/advert.py
@@ -14,7 +14,7 @@ from app.database.db_connect import connect
 
 def create_advert(values: dict, images: list) -> int:
     query: str = """
-    INSERT INTO Adverts (lID, title, description, tennants, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10)
+    INSERT INTO Adverts (lID, title, price, description, tennants, image1, image2, image3, image4, image5, image6, image7, image8, image9, image10)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
 
