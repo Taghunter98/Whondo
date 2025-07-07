83c901336d6af534e7531e0b2a03bba82f475c72
= <=>
2025-07-07 08:43:09 +0100

    Unit testing

--- app/tests/test_pka.py
+++ app/tests/test_pka.py
@@ -21,6 +21,6 @@ class TestPropertyKeywordAdvert(unittest.TestCase):
     def testDataIsValid(self):
         data = self.setUpClass()
 
-        self.assertEqual(data[0], 105, f"Function is returning: {data}")
-        self.assertEqual(data[1], 130, f"Function is returning: {data}")
-        self.assertEqual(data[2], 105, f"Function is returning: {data}")
+        self.assertEqual(data[0][0], 105, f"Function is returning: {data}")
+        self.assertEqual(data[0][1], 130, f"Function is returning: {data}")
+        self.assertEqual(data[0][2], 105, f"Function is returning: {data}")
