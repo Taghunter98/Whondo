36ae5df4a5de50ea2a873aaa09ca4ec644b12f09
Sarit Samkumpim <saritsamkumpim@gmail.com>
2025-07-18 15:42:18 +0100

    add input folder

---
+++ app/static/jay-comps/comps/inputs/textarea.js
@@ -0,0 +1,47 @@
+import { Input } from "./input.js";
+
+export class Textarea extends Input {
+   
+    rows_;
+
+    set rows(newRows) {
+
+        this.rows_ = newRows;
+        this.update();
+    
+    }
+
+    get rows() { return this.rows_; }
+
+    beforeRender() { if (!this.rows_) this.rows_ = 6; }
+
+    createHTML() {
+
+        return /* html */ `
+        <div class="inputContainer">
+            <label style="color: var(--black80); font-size: 14px">${this.label_}</label>
+            <textarea class="inputValue areaInput" placeholder="${this.prompt_}" rows="${this.rows_}"></textarea>
+        </div>
+        `;
+    
+    }
+
+    createCSS() {
+        return [
+            {
+                class: "areaInput",
+                resize: "none",
+                height: 80,
+                widthPercent: 100,
+                borderVar: "border",
+                borderRadius: 8,
+                boxSizing: "border-box",
+                padding: [8, 12],
+                fontFamily: "Geist",
+            }
+        ];
+    }
+
+    static{ super.register(this); }
+
+}
