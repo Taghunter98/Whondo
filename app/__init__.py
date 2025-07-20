ee66a9419596fd0787bca26bdec2d1a4c1fa3bb9
Josh Bassett <bassettjosh397@gmail.com>
2025-07-20 09:18:49 +0100

    Fixed dev branch bug
    - Isolated and fixed import bug due to unseen merge issue

--- app/__init__.py
+++ app/__init__.py
@@ -124,6 +124,7 @@ def create_app() -> Flask:
     app.config["SESSION_COOKIE_HTTPONLY"] = True
     app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
     app.config["MAX_CONTENT_LENGTH"] = 16 * 1000 * 1000
+    app.config.from_mapping(DEBUG=True)
     # app.config["SESSION_COOKIE_SAMESITE"] = True
 
     Session(app)
