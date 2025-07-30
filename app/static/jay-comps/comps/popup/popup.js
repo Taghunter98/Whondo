4a489e35561cfc0b52872f52ca290e5934b0dad0
Sarit Samkumpim <saritsamkumpim@gmail.com>
2025-07-30 19:58:18 +0100

    add popup comp

---
+++ app/static/jay-comps/comps/popup/popup.js
@@ -0,0 +1,112 @@
+import { Comp } from "jay-comp";
+
+export class Popup extends Comp {
+    title_; paragraph_; text_;
+
+    set title(newTitle) {
+        this.title_ = newTitle;
+        this.update();
+    }
+    set paragraph(value) {
+        this.paragraph_ = value;
+        this.update();
+    }
+    set text(v) {
+        this.text_ = v;
+        this.update();
+    }
+    get paragraph() { return this.paragraph_; }
+    get title() { return this.title_; }
+    get text() { return this.text_ }
+
+    beforeRender() {
+        if (!this.title_) this.title_ = "Hello World";
+        if (!this.paragraph_) this.paragraph_ = "This is a paragraph";
+    }
+
+    createHTML() {
+        return /* html */ `
+            <div class="background">
+                <div class="container">
+                    <comp-icon class="icon"></comp-icon>
+                    <h3 class="head">${this.title_}</h3>
+                    <p class="dialog">${this.paragraph_}</p>
+                    <div class="button-wrapper">
+                    <comp-button class="btn"></comp-button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+createCSS() {
+        const effect = this.effect.slideUp(20);
+        const prop = this.effect.prop("slideUp", .5);
+
+        const background = {
+            class: "background",
+            display: "flex",
+            flexDirection: "column",
+            widthPercent: 100,
+            heightVh: 100,
+            justifyContent: "centre",
+            alignItems: "centre",
+            background: "rgba(0, 0, 0, 0.6)",
+            overflow: "hidden",
+            position: "fixed",
+            zIndex: 9999,
+            media: {
+                maxWidthBp: 600,
+                padding: 20,
+                width: "auto"
+            }
+        };
+
+        const container = {
+            class: "container",
+                display: "flex",
+                flexDirection: "column",
+                width: "auto",
+                maxWidth: 500,
+                padding: 20,
+                borderVar: "border",
+                borderRadius: 16,
+                gap: 10,
+                background: "white",
+                animation: prop,
+                marginTop: 0,
+                media: {
+                    maxWidthBp: 600,
+                }
+        };
+
+        const dialog = {
+                class: "dialog",
+                fontSize: 16,
+                textAlign: "start",
+                media: {
+                    maxWidthBp: 600,
+                    fontSize: 16
+                }
+            }
+
+        const icon = {
+                class: "icon",
+                display: "flex",
+                alignItems: "centre",
+                justifyContent: "centre",
+                colourVar: "black80",
+            }
+
+        return [background, container ,dialog, icon, effect];
+    }
+
+    afterRender() {
+        this.query("comp-button").text = this.text_;
+        this.query("comp-button").fill = true;
+        
+    }
+
+    static { Comp.register(this); }
+
+    
+}
