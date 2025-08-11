fcdd38d01c102f7a2640c8483e2a576ab0e9c248
Sarit Samkumpim <saritsamkumpim@gmail.com>
2025-08-11 11:21:53 +0100

    add pic-banner comp

---
+++ app/static/frontend/components/pic_banner.js
@@ -0,0 +1,99 @@
+import { Comp } from "jay-comp";
+
+export class PicBanner extends Comp{
+
+    buttonText_;
+
+    set btnText(v) {
+        this.btnText_ = v;
+        this.update();
+    }
+
+    get btnText() { return this.btnText_; }
+
+     async fetchProfile() {
+        const { ok, data, error } = await this.request(
+            "https://whondo.com/verify/me",
+            "GET"
+        );
+
+        if (!ok) throw new Error(error);
+
+        const pic = data.profilePicture;
+        return pic
+            ? `https://whondo.com/uploads?path=${pic}`
+            : "https://whondo.com/static/icons/Profile.png";
+    }
+
+    createHTML() {
+        const entry = this.fetchOnce("profilePic", () => this.fetchProfile());
+        return /* html */`
+        <div class="banner">
+            <div class="content">
+                <img class="profile" src="${entry.value}">
+                <h4 class="title"></h4>
+            </div>
+            <div class="action">
+                <comp-button class="btn"></comp-button>
+            </div>
+        </div>
+        `;
+    }
+
+     createCSS() {
+        return [
+            {
+                class: "banner",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "centre",
+                flexDirection: "row",
+                maxWidthPercent: 100,
+                background: "white",
+                borderRadius: 12,
+                padding: 20,
+                gap: 12,
+                boxSizing: "border-box",
+                media: {
+                    maxWidthBp: 600,
+                    flexDirection: "column",
+                    gap: 16,
+                    alignItems: "start"
+                }
+            },
+            {
+                class: "content",
+                flex: 1,
+                display: "flex",
+                alignItems: "centre",
+                justifyContent: "flex-start",
+                media: {
+                    maxWidthBp: 600,
+                    justifyContent: "centre",
+                }
+            },
+            {
+                class: "title",
+                fontWeight: "bold"
+            },
+            {
+                class: "action",
+                display: "flex",
+                gap: 12,
+                media: {
+                    maxWidthBp: 600,
+                    widthPercent: 100
+                }
+            }
+        ];
+    }
+
+    afterRender() {
+        const btn = this.query(".btn");
+       
+        btn.text = this.btnText_;
+
+        btn.addEventListener("click", () => btn.publish("btn-click"));
+    }
+
+}
